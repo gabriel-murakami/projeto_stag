22e4047d56e0ead2105197f3a3573a98143201b6
Gabriel <youse@c0278.local>
2019-12-04 12:48:52 -0300

    Modal de erro e alertas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,24 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    #Definir o direcionamento após o login
     def after_sign_in_path_for(resource)
         '/'
     end
 
+    #Definir o direcionamento após o cadastro
+    # def after_sign_up_path_for(resource)
+    #     '/'
+    # end
+
     protected
 
+    #Definir os parâmetros permitidos
     def configure_permitted_parameters
       attributes = [:email, :password, :current_password, :password_confirmation,
-    :pf_pj, :nome, :sobrenome, :cpf, :documento, :orgao_emissor, :estado_emissor,
-    :data_emissao, :nome_fantasia, :razao_social, :cnpj, :cep, :logradouro, :numero,
-    :complemento, :bairro, :cidade, :estado]
+        :pf_pj, :nome, :sobrenome, :cpf, :documento, :orgao_emissor, :estado_emissor,
+        :data_emissao, :nome_fantasia, :razao_social, :cnpj, :cep, :logradouro, :numero,
+        :complemento, :bairro, :cidade, :estado]
       devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
       devise_parameter_sanitizer.permit(:account_update, keys: attributes)
     end
