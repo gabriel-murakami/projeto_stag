60858e75e97ae516cea0da987e4d55f3c0000f99
Gabriel <youse@c0278.local>
2019-12-04 10:42:42 -0300

    Cadastro com todos os requisitos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,20 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :authenticate_user!
 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def after_sign_in_path_for(resource)
         '/'
+    end
+
+    protected
+
+    def configure_permitted_parameters
+      attributes = [:email, :password, :current_password, :password_confirmation,
+    :pf_pj, :nome, :sobrenome, :cpf, :documento, :orgao_emissor, :estado_emissor,
+    :data_emissao, :nome_fantasia, :razao_social, :cnpj, :cep, :logradouro, :numero,
+    :complemento, :bairro, :cidade, :estado]
+      devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+      devise_parameter_sanitizer.permit(:account_update, keys: attributes)
     end
 end
