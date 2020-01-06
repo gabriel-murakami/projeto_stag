9c8bd1ff9ed9ff89bdf16e661663bbb892fa7ce7
Gabriel <youse@c0278.local>
2020-01-06 15:36:29 -0300

    Redefinição de senha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
       attributes = [:email, :password, :current_password, :password_confirmation,
         :pf_pj, :nome, :sobrenome, :cpf, :documento, :orgao_emissor, :estado_emissor,
         :data_emissao, :nome_fantasia, :razao_social, :cnpj, :cep, :logradouro, :numero,
-        :complemento, :bairro, :cidade, :estado]
+        :complemento, :bairro, :cidade, :estado, :admin]
       devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
       devise_parameter_sanitizer.permit(:account_update, keys: attributes)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, controllers: { registrations: "registrations", passwords: "passwords" }
 
   scope "/home" do
     get "/list", to: "home#list"
   end
 
   get "/home/sucesso"
+
+  get "/home/sucesso_senha"
 
   root "home#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
