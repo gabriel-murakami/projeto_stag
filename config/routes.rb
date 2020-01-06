3f3fa584727525ac168e1145e8d215d28925ee40
JopaFM <joao.paulo.fm@hotmail.com>
2020-01-06 16:21:40 -0300

    Jpm cadastro (#13)
    * revertendo ao commit de validação de email
    
    * Mudança para o SQLite
    
    * Limitando a quantidade de caracteres nos campos e Ctlr-C no Email (#7)
    
    * validação do email no login
    
    * Limitando tamanho dos campos
    
    * Limitando tamanho dos campos
    
    * Arrumando tamanho dos campos e Limitando o ctrl-c no campo Email
    
    * Atualiza cadastro
    
    * Validação de CPF, CNPJ e e-mail exclusivos
    
    * Validação CPF, CNPJ e e-mail exclusivos
    
    Co-authored-by: gabriel-murakami <56936297+gabriel-murakami@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
 
   get "/home/sucesso"
 
+  devise_scope :user do
+    post '/checkemail', to: 'registrations#emailcheck'
+    post '/checkcpf', to: 'registrations#cpfcheck'
+    post '/checkcnpj', to: 'registrations#cnpjcheck'
+  end 
+
   root "home#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
