8a282f8645468f2675be7cc25a949eb477220598
gabriel-murakami <56936297+gabriel-murakami@users.noreply.github.com>
2020-01-15 16:13:15 -0300

    Review Geral (#31)
    * Removendo o controller de senha
    
    * Review geral retirando partes em desuso e comentando códigos sem explicação
    
    Co-authored-by: JopaFM <joao.paulo.fm@hotmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,7 @@ class User < ApplicationRecord
 
   validates_with ::ScorpValidator
   validates :email, format: { :with => /\w\@gmail.com/ }, uniqueness: true
-  # validates :nome, format: { :with => /[a-zA-Z\u00C0-\u00FF ]+/i }
-  # validates :sobrenome, format: { :with => /[a-zA-Z\u00C0-\u00FF ]+/i }
-  # validates :orgao_emissor, format: { :with => /[a-zA-Z\u00C0-\u00FF ]+/i }
-  # validates :nome_fantasia, format: { :with => /[a-zA-Z\u00C0-\u00FF ]+/i }
-  # validates :razao_social, format: { :with => /[a-zA-Z\u00C0-\u00FF ]+/i }
-  # validates :cnpj, format: { :with => /\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}/ }, length: { :maximum => 18 }, uniqueness: true
-  # validates :cep, format: { :with => /\d{5}\-\d{3}/ }, length: { :maximum => 9 }
-  # validates :cpf, format: { :with => /\d{3}\.\d{3}\.\d{3}\-\d{2}/ }, length: { :maximum => 14 }, uniqueness: true
-  # validates :logradouro, format: { :with => /[a-zA-Z\u00C0-\u00FF ]+/i }, length: { :maximum => 40 }
-  # validates :bairro, format: { :with => /[a-zA-Z\u00C0-\u00FF ]+/i }, length: { :maximum => 30 }
-  # validates :cidade, format: { :with => /[a-zA-Z\u00C0-\u00FF ]+/i }, length: { :maximum => 30 }
-  # validates :complemento, format: { :with => /[a-zA-Z\u00C0-\u00FF ]+/i }, length: { :maximum => 10 }
+
 
   def self.searchEmail(email)
     if email
